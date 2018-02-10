3123a2a0069d81d0e50ba6a51d279295e37bfba0
Neyva Hernandez <neyvahernandez@Neyvas-MacBook-Air.local>
2018-02-10 17:46:30 +0400

    “updated

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("sup")
+
+let myVariable = "hello"
+
+let myHTMLBody = document.HTMLBody
+console.log(myHTMLBody)
+
+let myBlock = document.getElementById('myFirstBlock')
+console.log(myBlock)
+//Declared my first block in html
+
+
+let myNumberOfClicks = 0
+myNumberOfClicks ++
+if(myNumberOfClicks ==1)
+  console.log('you have clicked:' + myNumberOfClicks +'time')
+else
+    console.log('you have clicked:' + myNumberOfClicks +'times')
+
+
+myBlock.onclick = () => {
+myBlock.style.hight = '0px'
+
+let myVideo = document.getElementById('myVideo')
+myVideo.style.display = 'block'
+
+setTimeout()
+}
