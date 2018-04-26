c8f2f0178cbdaab15b5fa6b530de6b28b347a36f
Neyva Hernandez <neyvahernandez@Neyvas-MacBook-Air.local>
2018-04-26 12:50:00 +0400

    lol I have 2 buttons I'm sorry Pierre

---
+++ CodingChallenge/script.js
@@ -0,0 +1,66 @@
+
+
+function play(){
+   var audio = document.getElementById("AaronDoesIt");
+   var isPlaying = true;
+       audio.play();
+     }
+
+     function togglePause() {
+
+       var isPlaying = true;
+        var audio = document.getElementById("AaronDoesIt");
+       if (currentlyPlaying) {
+         audio.pause()
+       } else {
+         audio.play();
+       }
+     };
+
+     audio.onplaying = function() {
+       currentlyPlaying = true;
+     };
+     audio.onpause = function() {
+       currentlyPlaying = false;
+     };
+
+
+
+
+// console.log("sup")
+//
+// let myVariable = "hello"
+//
+// let myHTMLBody = document.HTMLBody
+// console.log(myHTMLBody)
+//
+// let myBlock = document.getElementById('myFirstBlock')
+// console.log(myBlock)
+// //Declared my first block in html
+//
+//
+// let myNumberOfClicks = 0
+// myNumberOfClicks ++
+// if(myNumberOfClicks ==1)
+//   console.log('you have clicked:' + myNumberOfClicks +'time')
+// else
+//     console.log('you have clicked:' + myNumberOfClicks +'times')
+//
+//
+// myBlock.onclick = () => {
+// myBlock.style.hight = '0px'
+//
+// let myVideo = document.getElementById('myVideo')
+// myVideo.style.display = 'block'
+//
+// myBlock.onclick = () => {
+//   myBlock.style.height = '0px'
+//
+//   setTimeout( () => {
+//     let myVideo = document.getElementById('myVideo')
+//     myVideo.style.display = 'block'
+//     setTimeout(myVideo.play(), 300)
+//   }, 1000)
+//
+// setTimeout()
+// }
