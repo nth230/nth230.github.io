585691cc28cf94e346a9626dbc3ae05624c0f57a
Neyva Hernandez <neyvahernandez@Neyvas-MacBook-Air.local>
2018-05-10 06:18:28 +0400

    finished for now

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,3 +1,31 @@
+// Side note; java script didn't work
+
+var modal = document.getElementById('simpleModal');
+
+var modalBtn = document.getElementById('modalBtn');
+
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+modalBtn.addEventListener("hover",openModal);
+
+closeBtn.addEventListener("hover",closeModal);
+
+window.addEventListener("click",clickOutside);
+
+function openModal(){
+  modal.style.display = "block";
+}
+
+function closeModal(){
+  modal.style.display = "none";
+}
+
+function clickOutside(e){
+  if(e.target == modal){
+  modal.style.display = "none";
+}
+//
+
 console.log("sup")
 
 let myVariable = "hello"
